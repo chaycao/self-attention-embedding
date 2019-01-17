e3ea9fdd1a9acad0bcd3c6901d7da2639722b3b5
chaycao <429629038@qq.com>
2019-01-17 20:38:17 +0800

    增加多种正则项

--- regularizers.py
+++ regularizers.py
@@ -1,7 +1,20 @@
 from keras import backend as K
 import tensorflow as tf
 
-def self_attentive_reg(A):
+def self_attentive_reg_5(A):
+    '''
+    自注意力编码的惩罚项
+    尽量让A的每一行不一样，获取更多不同的信息
+    :param A:
+    :return:
+    '''
+    x = K.batch_dot(A, tf.transpose(A, [0,2,1]))
+    x = x-1
+    x = tf.norm(x)
+    x = 0.00001*x*x
+    return x
+
+def self_attentive_reg_4(A):
     '''
     自注意力编码的惩罚项
     尽量让A的每一行不一样，获取更多不同的信息
@@ -12,4 +25,30 @@ def self_attentive_reg(A):
     x = x-1
     x = tf.norm(x)
     x = 0.0001*x*x
+    return x
+
+def self_attentive_reg_3(A):
+    '''
+    自注意力编码的惩罚项
+    尽量让A的每一行不一样，获取更多不同的信息
+    :param A:
+    :return:
+    '''
+    x = K.batch_dot(A, tf.transpose(A, [0,2,1]))
+    x = x-1
+    x = tf.norm(x)
+    x = 0.001*x*x
+    return x
+
+def self_attentive_reg_2(A):
+    '''
+    自注意力编码的惩罚项
+    尽量让A的每一行不一样，获取更多不同的信息
+    :param A:
+    :return:
+    '''
+    x = K.batch_dot(A, tf.transpose(A, [0,2,1]))
+    x = x-1
+    x = tf.norm(x)
+    x = 0.01*x*x
     return x
